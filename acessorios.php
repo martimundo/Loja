4eb8c8224ea1e82e8e21248e3caccac54436f3bf
Marcos <martimundo@gmail.com>
2020-10-21 22:21:09 -0300

    Projeto de Loja virtualn W51-agency

---
+++ acessorios.php
@@ -0,0 +1,21 @@
+<?php
+   require __DIR__.'/vendor/autoload.php';
+   
+   include __DIR__.'/includes/header.php';
+   
+?>
+<div class="product-big-title-area">
+   <div class="container">
+      <div class="row">
+         <div class="col-md-12">
+            <div class="product-bit-title text-center">
+               <h2>Acessórios</h2>
+            </div>
+         </div>
+      </div>
+   </div>
+</div>
+
+
+
+<?php include __DIR__. '/includes/footer.php';?>
